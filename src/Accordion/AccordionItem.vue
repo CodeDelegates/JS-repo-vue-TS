73427e344c6beeea89cfa80a84de65310e87fa19
Code Delegates <CodeDelegates@gmail.com>
2025-01-06 22:56:10 +0100

    Working Test

---
+++ src/Accordion/AccordionItem.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+	import { AccordionItem, useForwardProps } from 'radix-vue';
+	import type { AccordionItemProps } from 'radix-vue';
+	import { computed } from 'vue';
+	import type { HTMLAttributes } from 'vue';
+	import { cn } from '../Utils';
+
+	const props = defineProps<AccordionItemProps & { class?: HTMLAttributes['class'] }>();
+
+	const delegatedProps = computed(() => {
+		const { class: _, ...delegated } = props;
+
+		return delegated;
+	});
+
+	const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+	<AccordionItem v-bind="forwardedProps" :class="cn('border-b', props.class)">
+		<slot />
+	</AccordionItem>
+</template>
---
+++ src/Accordion/AccordionTrigger.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+	import { AccordionHeader, AccordionTrigger } from 'radix-vue';
+	import type { AccordionTriggerProps } from 'radix-vue';
+	import { computed } from 'vue';
+	import type { HTMLAttributes } from 'vue';
+	import { cn } from '../Utils';
+
+	const props = defineProps<AccordionTriggerProps & { class?: HTMLAttributes['class'] }>();
+
+	const delegatedProps = computed(() => {
+		const { class: _, ...delegated } = props;
+
+		return delegated;
+	});
+</script>
+
+<template>
+	<AccordionHeader class="flex">
+		<AccordionTrigger
+			v-bind="delegatedProps"
+			:class="
+				cn(
+					'flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180',
+					props.class,
+				)
+			"
+		>
+			<slot />
+			<slot name="icon"> </slot>
+		</AccordionTrigger>
+	</AccordionHeader>
+</template>
